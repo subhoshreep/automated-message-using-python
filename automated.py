71a61a9a8cb74942069d6af6042f5f62f0976828
SUBHOSHREE PATTNAIK <76872340+subhoshreep@users.noreply.github.com>
2021-03-15 22:23:12 +0530

    Update automated.py

--- automated.py
+++ automated.py
@@ -2,6 +2,6 @@ import pyautogui
 import time
 while True:
     time.sleep(3)
-    pyautogui.typewrite('hello joy dada,program trial!')
+    pyautogui.typewrite('hello,program trial!')
     pyautogui.press('enter')
 
